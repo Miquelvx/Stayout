bfe43bd7249c8909a7f2c307f4cf96baedfac3d1
Mique <72611136+Miquelvx@users.noreply.github.com>
2026-07-22 14:24:25 +0200

    Update Prédiction_de_course_⚙️.py

--- pages/Prédiction_de_course_⚙️.py
+++ pages/Prédiction_de_course_⚙️.py
@@ -195,6 +195,7 @@ def main():
                 # --- 1. Initialiser les données du GP actuel pour la prédiction ---
                 try:
                     df_next_gp = initialize_feature_df_qualif(2026, next_event['RoundNumber'], constructors_df)
+                    print(next_event['RoundNumber'])
                 except ValueError as e:
                     st.warning(str(e))
                     st.stop()
